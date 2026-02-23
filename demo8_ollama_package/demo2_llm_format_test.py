f77bed0ab30ffe73f2522e3ca815ba9a3224807d
balaji-githubstore <balajidinakaran1@gmail.com>
2026-02-23 11:22:18 +0530

    Automation check on llm output

--- demo8_ollama_package/demo2_llm_format_test.py
+++ demo8_ollama_package/demo2_llm_format_test.py
@@ -1,6 +1,8 @@
 # pip install ollama
 import ollama
-
+import json
+import pandas as pd
+import re
 
 response = ollama.chat(model="gemma3:1b", 
                        messages=[
@@ -18,4 +20,17 @@ response = ollama.chat(model="gemma3:1b",
 actual_llm_output=response.message.content
 print(actual_llm_output)
 
- 
+actual_cleared_data=re.sub(r"^```(?:json)?|```$","",actual_llm_output,flags=re.MULTILINE).strip()
+print(actual_cleared_data)
+
+# json object --> make sure it is in proper json format
+data=json.loads(actual_cleared_data)
+
+# pass list or dic --> here passing list of json object
+df=pd.DataFrame([data])
+
+print(df)
+
+print(df.columns)
+
+# will start at 
